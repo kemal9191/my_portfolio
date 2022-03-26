5c03252037665187a35372bfa6d605c3d6ced4cc
kemal9191 <kpehlivan91@gmail.com>
2022-03-26 17:42:40 +0300

    code snipper added

--- app/config.py
+++ app/config.py
@@ -5,3 +5,5 @@ import binascii
 class Config:
     SECRET_KEY = binascii.b2a_hex(os.urandom(15))
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost:5432/my-port'
+    CKEDITOR_ENABLE_CODESNIPPET = True
+    CKEDITOR_CODE_THEME = "default"
